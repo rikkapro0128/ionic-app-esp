aa4405d9ba209694d156a97c15de73cc056e3089
rikkapro0128 <stkssc002@gmail.com>
2022-11-26 00:36:10 +0700

    save code

--- src/App.tsx
+++ src/App.tsx
@@ -39,45 +39,59 @@ import './index.css';
 
 import { useEffect, useState } from 'react';
 import { WifiWizard2 } from '@awesome-cordova-plugins/wifi-wizard-2';
-import { IconGoogle } from './icons';
+
+import { IconGoogle, IconEmptyWifi } from './icons';
+
 import Box from '@mui/material/Box';
 import WifiIcon from '@mui/icons-material/Wifi';
 import Typography from '@mui/material/Typography';
 import SignalCellular1BarIcon from '@mui/icons-material/SignalCellular1Bar';
 import SignalCellular2BarIcon from '@mui/icons-material/SignalCellular2Bar';
 import SignalCellular3BarIcon from '@mui/icons-material/SignalCellular3Bar';
+import SignalCellular4BarIcon from '@mui/icons-material/SignalCellular4Bar';
 import Button from '@mui/material/Button';
 import Zoom from '@mui/material/Zoom';
-
+import Fade from '@mui/material/Fade';
+import Grow from '@mui/material/Grow';
 
 setupIonicReact();
+
+function checkLevelSignal(level: number) {
+  let convert = level * (-1);
+  if(convert > 30 && convert < 50) {
+    // signal large
+    return <SignalCellular4BarIcon />
+  }else if(convert >= 50 && convert < 70) {
+    // signal medium - 2
+    return <SignalCellular3BarIcon />
+  }else if(convert >= 70 && convert < 90) {
+    // signal medium - 1
+    return <SignalCellular2BarIcon />
+  }
+  else {
+    // signal small
+    return <SignalCellular1BarIcon />
+  }
+}
 
 const App: React.FC = () =>{
 
   const [scan, setScan] = useState(false);
+  const [networks, setNetworks] = useState([]);
 
-  useEffect(() => {
-    const run = async () => {
-      // const stateScan = await WifiWizard2.startScan();
-      // if(stateScan) {
-      //   const networks = await WifiWizard2.getScanResults({ numLevels: 0 });
-      //   console.log(networks);
-      // }
+  async function activeScan() {
+    try {
+      if(!scan) {
+        setScan(true)
+        const networks = await WifiWizard2.scan({ numLevels: 0 });
+        setNetworks(networks);
+        setScan(false);
+        console.log(networks);
+      }
+    } catch (error) {
+      console.log(error);
+      setScan(false);
     }
-    run();
-  }, []);
-
-  useEffect(() => {
-    if(scan) {
-      // const id = setInterval(() => {
-
-      // }, )
-    }
-  }, [scan])
-
-  function activeScan() {
-    setScan(!scan)
-    // setScan(true)
   }
 
   return (
@@ -109,32 +123,47 @@ const App: React.FC = () =>{
           {/* <Zoom in={!scan} style={{ transitionDelay: '500ms', }}>
           </Zoom> */}
         </Box>
-        <Box className='bg-white relative w-full flex-1 rounded-t-3xl p-6 shadow-2xl'>
-          <Typography variant="h5" className='text-slate-600'>
-            Danh sách node
-          </Typography>
-          <Typography variant="subtitle1" className='text-slate-600'>
-            5 node
-          </Typography>
+        <Box className='max-h-[530px] bg-white flex flex-col relative w-full flex-1 rounded-t-3xl p-6 shadow-2xl'>
           <Box>
-            <Button sx={{ display: 'flex', justifyContent: 'space-between', padding: '10px 0', textTransform: 'unset' }} className='flex-nowrap w-full'>
-              <Typography variant="h6" className='text-slate-600'>
-                Truonghair22
-              </Typography>
-              <SignalCellular3BarIcon /> 
-            </Button>
-            <Button sx={{ display: 'flex', justifyContent: 'space-between', padding: '10px 0', textTransform: 'unset' }} className='flex-nowrap w-full'>
-              <Typography variant="h6" className='text-slate-600'>
-                huy ngoc
-              </Typography>
-              <SignalCellular2BarIcon /> 
-            </Button>
-            <Button sx={{ display: 'flex', justifyContent: 'space-between', padding: '10px 0', textTransform: 'unset' }} className='flex-nowrap w-full'>
-              <Typography variant="h6" className='text-slate-600'>
-                duy123
-              </Typography>
-              <SignalCellular1BarIcon /> 
-            </Button>
+            <Typography variant="h5" className='text-slate-600'>
+              Danh sách node
+            </Typography>
+            <Typography variant="subtitle1" className='text-slate-600'>
+              { networks.length } node
+            </Typography>
+          </Box>
+          <Box className='flex-1 overflow-y-scroll my-2'>
+            {
+              networks.length
+              ?
+              <Fade in={true}>
+                <Box>
+                  {
+                    networks.map(network => (
+                      <Button key={network['BSSID']} sx={{ display: 'flex', justifyContent: 'space-between', padding: '10px 0', textTransform: 'unset' }} className='flex-nowrap w-full'>
+                        <Typography variant="h6" className='text-slate-600'>
+                          { network['SSID'] }
+                        </Typography>
+                        { checkLevelSignal(network['level']) }
+                      </Button>
+                    ))
+                  }
+                </Box>
+              </Fade>
+              :
+              <Grow
+                in={true}
+                style={{ transformOrigin: '0 0 0' }}
+                {...(networks.length > 0 ? { timeout: 1000 } : {})}
+              >
+                <Box className='flex flex-col items-center justify-center h-full'>
+                  <IconEmptyWifi className='w-32 h-32' />
+                  <Typography variant="subtitle2" className='text-slate-600 text-center'>
+                    wifi đang trống nhấn quét để tìm kiếm xung quanh.
+                  </Typography>
+                </Box>
+              </Grow>
+            }
           </Box>
         </Box>
       </Box>
