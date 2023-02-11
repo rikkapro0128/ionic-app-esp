c86482d93b8e343f60700f41b4ebf2d424304507
rikkapro0128 <stkssc002@gmail.com>
2023-02-11 16:13:10 +0700

    save code

--- src/components/Status/Wifi.tsx
+++ src/components/Status/Wifi.tsx
@@ -1,30 +1,65 @@
-import { memo } from "react";
-import { WifiPresent } from '../../pages/Connect';
-import { IconNode } from '../../icons/index';
+import { memo, useState } from "react";
+import { WifiPresent , WifiInfo} from "../../pages/Connect";
+import { IconNode } from "../../icons/index";
 
 interface PropsType {
-  end: boolean,
-  bssid: string,
-  ssid: string,
-  type: number,
-  quality: number,
-  present?: WifiPresent,
-  onClick: (ssid: string, bssid: string) => void,
+  end: boolean;
+  payload: WifiInfo;
+  present?: WifiPresent;
+  onClick: (wifi: WifiInfo) => void;
+  onConfig?: (wifi: WifiInfo) => void;
 }
 
-const StatusWifi = ({ bssid, quality, onClick, ssid, type, end, present }: PropsType) => {
+const StatusWifi = ({
+  payload,
+  onClick,
+  end,
+  present,
+  onConfig,
+}: PropsType) => {
+  const [idTimeOut, setIdTimeOut] = useState<NodeJS.Timeout>();
+
+  const onMouseUp = () => {
+    // console.log('Press Up');
+    console.log('clear', idTimeOut);
+    
+    clearTimeout(idTimeOut);
+  };
+  const onMouseDown = () => {
+      // console.log('Press Down');
+    const id = setTimeout(() => {
+      if(typeof onConfig === 'function' && present?.BSSID === payload.BSSID) { onConfig(payload); }
+    }, 1000);
+    setIdTimeOut(id);
+  };
+
   return (
-    <div onClick={() => { onClick(ssid, bssid) }} className={`py-4 active:bg-slate-200 transition-colors grid grid-cols-6 ${end ? '' : 'border-t-[1px]'}`}>
+    <div
+      onTouchStart={onMouseDown}
+      onTouchEnd={onMouseUp}
+      onClick={() => {
+        onClick(payload);
+      }}
+      className={`py-4 active:bg-slate-200 transition-colors grid grid-cols-6 ${
+        end ? "" : "border-t-[1px]"
+      }`}
+    >
       <div className="col-span-3 flex mr-4">
-        <IconNode className='fill-slate-700 w-4 h-4 mr-4' />
-        <span className="flex-1 max-w-[150px] overflow-x-scroll whitespace-nowrap text-ellipsis">{ ssid || 'NaN' }</span>
+        <IconNode className="fill-slate-700 w-4 h-4 mr-4" />
+        <span className="flex-1 max-w-[150px] overflow-x-scroll whitespace-nowrap text-ellipsis">
+          {payload.SSID || "NaN"}
+        </span>
       </div>
       <div className="col-span-3 flex justify-between items-center">
-        <span>{ type ? type > 2000 && type < 3000 ? '2.4' : '5' : 'NaN' }Ghz</span>
-        {
-          present?.BSSID === bssid  ? <span className="border-[1px] border-green-600 text-green-600 py-1 px-2 rounded-full text-xs">connected</span> : null
-        }
-        <span>{ Math.min(Math.max(2 * (quality + 100), 0), 100) || 0 }%</span>
+        <span>
+          {payload.frequency ? (payload.frequency > 2000 && payload.frequency < 3000 ? "2.4" : "5") : "NaN"}Ghz
+        </span>
+        {present?.BSSID === payload.BSSID ? (
+          <span className="border-[1px] border-green-600 text-green-600 py-1 px-2 rounded-full text-xs">
+            connected
+          </span>
+        ) : null}
+        <span>{Math.min(Math.max(2 * (payload.level + 100), 0), 100) || 0}%</span>
       </div>
     </div>
   );
--- src/pages/Connect/index.tsx
+++ src/pages/Connect/index.tsx
@@ -20,15 +20,15 @@ import { Box } from "@mui/material";
 import BtnScan from "../../components/Btn/Scan";
 import StatusWifi from "../../components/Status/Wifi";
 
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import TextField from '@mui/material/TextField';
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import TextField from "@mui/material/TextField";
 
-interface WifiInfo {
+export interface WifiInfo {
   BSSID: string;
   SSID: string;
   capabilities: string;
@@ -44,6 +44,9 @@ export interface WifiPresent {
   SSID: string;
 }
 
+interface WifiNode extends WifiPresent {}
+interface WifiStation extends WifiPresent {}
+
 const notify = ({ title = "Thông báo", body = "Push notìy thành công!" }) =>
   toast.custom((t) => <Notify title={title} body={body} state={t} />, {
     duration: 5000,
@@ -51,10 +54,18 @@ const notify = ({ title = "Thông báo", body = "Push notìy thành công!" }) =
 
 function Connect() {
   const [wifiPresent, setWifiPresent] = useState<WifiPresent>();
+  const [wifiTarget, setWifiTarget] = useState<string>(
+    () => localStorage.getItem("wifi-target") || ""
+  );
   const [wifis, setWifis] = useState<Array<WifiInfo>>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [openDiaNode, setOpenDiaNode] = useState<boolean>(false);
-  const [password, setPassword] = useState<string>('');
+  const [openDiaNodeTarget, setOpenDiaNodeTarget] = useState<boolean>(false);
+  const [passwordNode, setPasswordNode] = useState<string>("");
+  const [pickNode, setPickNode] = useState<WifiNode>();
+  const [pickStation, setPickStation] = useState<WifiStation>();
+  const [passwordStation, setPasswordStation] = useState<string>("");
+  const [passwordTarget, setPasswordTarget] = useState<string>("");
 
   useEffect(() => {
     let idTimeOut: NodeJS.Timeout;
@@ -70,6 +81,16 @@ function Connect() {
       clearInterval(idTimeOut);
     };
   }, []);
+
+  useEffect(() => {
+    if (!wifiTarget && wifiPresent?.SSID) {
+      setHandleWifiTarget(wifiPresent.SSID);
+    }
+  }, [wifiPresent]);
+
+  useEffect(() => {
+    handleConnectToStation(pickStation);
+  }, [pickStation]);
 
   const clickScan = useCallback(async () => {
     // excute scan wifi
@@ -90,22 +111,99 @@ function Connect() {
     setLoading(false);
   }, []);
 
-  const wifiPickHander = useCallback(async (ssid: string, bssid: string) => {
+  const wifiPickHander = useCallback(async (wifi: WifiInfo) => {
     // excute scan wifi
-    console.log(ssid, bssid);
-    if (ssid.includes("esp")) {
+    console.log(wifi.SSID, wifi.BSSID);
+    if (wifi.SSID.includes("esp")) {
+      setPickNode({ SSID: wifi.SSID, BSSID: wifi.BSSID });
       onOpenDiaNode();
     } else {
+      setPickStation({ SSID: wifi.SSID, BSSID: wifi.BSSID });
     }
+  }, []);
+
+  const configWifiToNode = useCallback(async (wifi: WifiInfo) => {
+    console.log(wifi);
+    onOpenDiaNodeTarget();
   }, []);
 
   const onOpenDiaNode = () => {
     setOpenDiaNode(true);
-  }
+  };
+  const onOpenDiaNodeTarget = () => {
+    setOpenDiaNodeTarget(true);
+  };
   const onCloseDiaNode = () => {
     setOpenDiaNode(false);
-    setPassword('');
-  }
+    setPasswordNode("");
+  };
+  const onCloseDiaNodeTarget = () => {
+    setOpenDiaNodeTarget(false);
+    setPasswordTarget("");
+  };
+
+  const handleConnectToStation = async (wifi: WifiStation | undefined) => {
+    console.log(pickStation);
+
+    if (wifi?.SSID && wifi?.BSSID) {
+      try {
+        let networksSaved: string[] = await WifiWizard2.listNetworks();
+        networksSaved = networksSaved.map((network) =>
+          network.replaceAll('"', "")
+        );
+        console.log(networksSaved);
+
+        const state = await WifiWizard2.connect(
+          wifi?.SSID,
+          true,
+          passwordStation ? passwordStation : undefined,
+          "WPA"
+        );
+        console.log(state);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const handleConnectToNode = async () => {
+    if (pickNode?.SSID && pickNode?.BSSID) {
+      try {
+        const state = await WifiWizard2.connect(
+          pickNode?.SSID,
+          true,
+          passwordNode,
+          "WPA"
+        );
+        console.log(state); // NETWORK_CONNECTION_COMPLETED -
+      } catch (error) {
+        console.log(error);
+      }
+      onCloseDiaNode();
+    }
+  };
+
+  const hanldeConnectFromNodeToTarget = async () => {
+    try {
+      if(wifiPresent) {
+        const result = await CapacitorHttp.post({
+          url: `http://192.168.4.1/config-wifi`,
+          data: JSON.stringify({ ssid: wifiPresent.SSID, password: passwordTarget }),
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+        });
+        notify({ body: `Cấu hình WIFI ${wifiTarget} cho ${wifiPresent.SSID} thành công.` });
+      }
+    } catch (error) {
+      console.log(error);notify({ body: 'Có lỗi xảy ra khi cấu hình WIFI!', title: 'Lỗi rồi' });
+    }
+    onCloseDiaNodeTarget();
+  };
+
+  const setHandleWifiTarget = (ssid: string) => {
+    setWifiTarget(ssid);
+    localStorage.setItem("wifi-target", ssid);
+  };
 
   return (
     <>
@@ -116,16 +214,51 @@ function Connect() {
         aria-describedby="alert-dialog-node-description"
       >
         <DialogTitle id="alert-dialog--title">
-          {`Thiết lập kết nối wifi đến ${wifiPresent?.SSID}?`}
+          {`Thiết lập kết nối đến ${pickNode?.SSID}?`}
         </DialogTitle>
-        <DialogContent style={{ paddingTop: '1rem' }}>
-          <TextField fullWidth value={password} onChange={(event) => { setPassword(event.target.value) }} id="outlined-node-basic" label="Mật khẩu" variant="outlined" focused={true} />
+        <DialogContent style={{ paddingTop: "1rem" }}>
+          <TextField
+            fullWidth
+            value={passwordNode}
+            onChange={(event) => {
+              setPasswordNode(event.target.value);
+            }}
+            id="outlined-node-basic"
+            label="Mật khẩu"
+            variant="outlined"
+            focused={true}
+          />
         </DialogContent>
         <DialogActions>
           <Button onClick={onCloseDiaNode}>huỷ</Button>
-          <Button onClick={onCloseDiaNode}>
-            kết nối
-          </Button>
+          <Button onClick={handleConnectToNode}>kết nối</Button>
+        </DialogActions>
+      </Dialog>
+      <Dialog
+        open={openDiaNodeTarget}
+        onClose={onCloseDiaNodeTarget}
+        aria-labelledby="alert-dialog-node-wifi-setup-title"
+        aria-describedby="alert-dialog-node-wifi-setup-description"
+      >
+        <DialogTitle id="alert-dialog--title">
+          {`Cấu hình kết nối ${pickNode?.SSID} tới wifi ${wifiTarget}?`}
+        </DialogTitle>
+        <DialogContent style={{ paddingTop: "1rem" }}>
+          <TextField
+            fullWidth
+            value={passwordTarget}
+            onChange={(event) => {
+              setPasswordTarget(event.target.value);
+            }}
+            id="outlined-node-wifi-setup-basic"
+            label="Mật khẩu"
+            variant="outlined"
+            focused={true}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onCloseDiaNodeTarget}>huỷ</Button>
+          <Button onClick={hanldeConnectFromNodeToTarget}>kết nối</Button>
         </DialogActions>
       </Dialog>
       <div style={{ height: "100vh" }} className="bg-slate-100 flex flex-col">
@@ -139,8 +272,8 @@ function Connect() {
             </h2>
             <h2 className="text-slate-700 text-md uppercase font-bold">
               thiết lập cho:
-              <span style={{ textTransform: 'none' }} className="font-normal">
-                {" " + wifiPresent?.SSID || "chưa thiết lập"}
+              <span style={{ textTransform: "none" }} className="font-normal">
+                {" " + wifiTarget || "chưa thiết lập"}
               </span>
             </h2>
           </div>
@@ -158,13 +291,13 @@ function Connect() {
                 wifis.map((wifi, index) => (
                   <StatusWifi
                     present={wifiPresent}
+                    onConfig={
+                      wifi.SSID.includes("esp") ? configWifiToNode : undefined
+                    }
                     end={index === 0 ? true : false}
                     onClick={wifiPickHander}
-                    type={wifi.frequency}
-                    ssid={wifi.SSID}
+                    payload={wifi}
                     key={wifi.SSID + wifi.BSSID}
-                    bssid={wifi.BSSID}
-                    quality={wifi.level}
                   />
                 ))
               )
